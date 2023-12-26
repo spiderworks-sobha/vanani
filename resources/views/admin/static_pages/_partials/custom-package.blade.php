0ec6abbff202530785ecf40e57040b880454b80c
spiderworks-sobha <sobha@spiderworks.in>
2023-12-26 10:43:42 +0530

    added different background images for diffrerent sections in custom package

--- resources/views/admin/static_pages/_partials/custom-package.blade.php
+++ resources/views/admin/static_pages/_partials/custom-package.blade.php
@@ -25,7 +25,13 @@
         <div class="form-group col-md-12">
             <label>Title</label>
             <input type="text" name="content[section2_title]" class="form-control" @if($obj->content && isset($obj->content['section2_title'])) value="{{$obj->content['section2_title']}}" @endif >
-        </div>    
+        </div>
+        <div class="form-group col-md-12">
+            @php
+                $section2_banner_media_id = ($obj->content && isset($obj->content['section2_banner_media_id']))?$obj->content['section2_banner_media_id']:null;
+            @endphp
+            @include('admin.media.set_file', ['file'=>$section2_banner_media_id, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[section2_banner_media_id]', 'id'=>'section2_banner_media_id', 'display'=> 'horizontal'])                                             
+        </div>
     </fieldset><!--end fieldset-->
     <h3>Section 3</h3>
     <fieldset class="row">                                                                               
@@ -33,6 +39,12 @@
             <label>Title</label>
             <input type="text" name="content[section3_title]" class="form-control" @if($obj->content && isset($obj->content['section3_title'])) value="{{$obj->content['section3_title']}}" @endif >
         </div>    
+        <div class="form-group col-md-12">
+            @php
+                $section3_banner_media_id = ($obj->content && isset($obj->content['section3_banner_media_id']))?$obj->content['section3_banner_media_id']:null;
+            @endphp
+            @include('admin.media.set_file', ['file'=>$section3_banner_media_id, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[section3_banner_media_id]', 'id'=>'section3_banner_media_id', 'display'=> 'horizontal'])                                             
+        </div>
     </fieldset><!--end fieldset-->                            
     <h3>Section 4</h3>
     <fieldset class="row">                                                                               
@@ -43,13 +55,25 @@
         <div class="form-group col-md-12">
             <label>Title 2</label>
             <input type="text" name="content[section4_title2]" class="form-control" @if($obj->content && isset($obj->content['section4_title2'])) value="{{$obj->content['section4_title2']}}" @endif >
-        </div>    
+        </div>
+        <div class="form-group col-md-12">
+            @php
+                $section4_banner_media_id = ($obj->content && isset($obj->content['section4_banner_media_id']))?$obj->content['section4_banner_media_id']:null;
+            @endphp
+            @include('admin.media.set_file', ['file'=>$section4_banner_media_id, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[section4_banner_media_id]', 'id'=>'section4_banner_media_id', 'display'=> 'horizontal'])                                             
+        </div>   
     </fieldset><!--end fieldset-->
     <h3>Section 5</h3>
     <fieldset class="row">                                                                               
         <div class="form-group col-md-12">
             <label>Title</label>
             <input type="text" name="content[section5_title]" class="form-control" @if($obj->content && isset($obj->content['section5_title'])) value="{{$obj->content['section5_title']}}" @endif >
-        </div>    
+        </div>
+        <div class="form-group col-md-12">
+            @php
+                $section5_banner_media_id = ($obj->content && isset($obj->content['section5_banner_media_id']))?$obj->content['section5_banner_media_id']:null;
+            @endphp
+            @include('admin.media.set_file', ['file'=>$section5_banner_media_id, 'title'=>'Banner Image', 'popup_type'=>'single_image', 'type'=>'Image', 'holder_attr'=>'content[section5_banner_media_id]', 'id'=>'section5_banner_media_id', 'display'=> 'horizontal'])                                             
+        </div>
     </fieldset><!--end fieldset-->       
 </div><!--end form-->
